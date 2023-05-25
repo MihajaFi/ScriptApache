fc6e3ba3897e38adf503abd58acbef39c087f8ae
Fifaliana <hei.fifaliana@gmail.com>
2023-05-25 13:41:58 +0300

    feat : script

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const domain = "hei.school" ;
 let nameOfDomain =  ["www" , "api" ,"front" ,"back"]  ;
 for (let i = 0; i < nameOfDomain.length; i++) {
 let domainName = `${nameOfDomain[i]}.${domain}`;
-    
+console.log(domainName);
 }
